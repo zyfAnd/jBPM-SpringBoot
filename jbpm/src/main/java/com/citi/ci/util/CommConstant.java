23c0fcf1001604b53ac746bb32af1bd81850a78c
yanfu <18842647603@163.com>
2018-07-30 23:44:49 +0800

    init for jBPM and SpringBoot

---
+++ jbpm/src/main/java/com/citi/ci/util/CommConstant.java
@@ -0,0 +1,11 @@
+package com.citi.ci.util;
+
+public class CommConstant {
+
+//	**********************************BASE_HOST*********************************
+	public static final String BAST_HOST = "http://localhost:8099";
+//	**********************************REDITLIMIT*********************************
+	public static final String CREDITLIMIT_URI = "/creditlimit";
+	public static final String TEMPORARYINCREASE_URI = "/temporaryIncrease";
+//	
+}
---
+++ jbpm/src/main/java/com/citi/ci/util/HttpUtil.java
@@ -0,0 +1,58 @@
+package com.citi.ci.util;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+/**
+ * 
+ * @author yanfu
+ *
+ */
+public class HttpUtil {
+
+	public static String httpGet(String  url) {  
+        CloseableHttpClient httpclient = HttpClients.createDefault();  
+        try {  
+            // 创建httpget.    
+            HttpGet httpget = new HttpGet(url);  
+            System.out.println("executing request " + httpget.getURI());  
+            // 执行get请求.    
+            CloseableHttpResponse response = httpclient.execute(httpget);  
+            try {  
+                // 获取响应实体    
+                HttpEntity entity = response.getEntity();  
+                // 打印响应状态    
+                System.out.println(response.getStatusLine());  
+                if (entity != null) {  
+                    // 打印响应内容长度    
+                    System.out.println("Response content length: " + entity.getContentLength());  
+                    // 打印响应内容    
+//                    System.out.println("Response content: " + EntityUtils.toString(entity));  
+                    return   EntityUtils.toString(entity);
+                }  
+            } finally {  
+//                response.close();  
+            }  
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        }  catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            // 关闭连接,释放资源    
+            try {  
+                httpclient.close();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+            }  
+        }
+		return null;  
+    }
+
+
+}
